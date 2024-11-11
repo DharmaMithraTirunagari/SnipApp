946e32be33ba6fa0e153843db648a519dfe3e0ae
DharmaMithra <dharmarockz2001@gmail.com>
2024-11-11 13:06:51 -0600

    Initial Commit

---
+++ SnipApp25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnipApp25
+//
+//  Created by Srikanth Kyatham on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SnipApp25Tests/SnipApp25Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SnipApp25Tests.swift
+//  SnipApp25Tests
+//
+//  Created by Srikanth Kyatham on 11/11/24.
+//
+
+import Testing
+@testable import SnipApp25
+
+struct SnipApp25Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
