ce28ca81429a50ec09c4242f9edb8733e2959163
DharmaMithra <dharmarockz2001@gmail.com>
2024-11-14 19:26:38 -0600

    settings Tab bar controller is added

--- SnipApp25/ContactTabVC.swift
+++ SnipApp25/ContactTabVC.swift
@@ -9,21 +9,50 @@ import UIKit
 
 class ContactTabVC: UIViewController {
 
+    @IBOutlet weak var submitButton: CustomButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addGradientBackground()
         // Do any additional setup after loading the view.
+        styleSubmitButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1,
+                           animations: {
+                               sender.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                           },
+                           completion: { _ in
+                               UIView.animate(withDuration: 0.1) {
+                                   sender.transform = CGAffineTransform.identity
+                               }
+                           })
     }
-    */
-
+    @IBAction func twitterButtonTapped(_ sender: UIButton) {
+        openSocialMedia(url: "https://www.twitter.com")
+    }
+    
+    @IBAction func facebookButtonTapped(_ sender: UIButton) {
+        openSocialMedia(url: "https://www.facebook.com")
+    }
+    
+    @IBAction func linkedinButtonTapped(_ sender: UIButton) {
+        openSocialMedia(url: "https://www.linkedin.com")
+    }
+    
+    private func openSocialMedia(url: String) {
+            if let socialURL = URL(string: url) {
+                UIApplication.shared.open(socialURL)
+            }
+        }
+        
+        private func styleSubmitButton() {
+            // Add styling for the Submit button (rounded corners, color, etc.)
+            submitButton.layer.cornerRadius = 10
+            submitButton.backgroundColor = UIColor.systemBlue
+            submitButton.tintColor = .white
+        }
+    
+    
 }
--- SnipApp25/LoginViewController.swift
+++ SnipApp25/LoginViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class LoginViewController : UIViewController {
     
+//    @IBAction func submitButtonPressed(_ sender: UIButton) {
+//        let welcomeViewController = self.storyboard?.instantiateViewController(withIdentifier: "WelcomeViewController") as? WelcomeViewController
+//        if let safeValue = welcomeViewController {
+//            self.navigationController?.pushViewController(safeValue, animated: true)
+//        }
+//    }
+    
+    
+    @IBAction func forgotPasswordPressed(_ sender: UIButton) {
+        let forgotPasswordViewController = self.storyboard?.instantiateViewController(withIdentifier: "ForgotPasswordViewController") as? ForgotPasswordViewController
+        if let safeValue = forgotPasswordViewController {
+            self.navigationController?.pushViewController(safeValue, animated: true)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
